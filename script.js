11efd02de27aab86b00a9cc523f7c84f8481cc94
miadehaan <64979457+miadehaan@users.noreply.github.com>
2020-08-05 18:03:43 -0700

    moved the user prompts inside of the generatePassword() function so that when the button is clicked, the user is then prompted with a series of criteria questions, then the password is generated

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ var lowLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m
 var upperLetters= []; //see for loop below
 var specialChar = ['@', '#', '$', '%', '/', '_', '*'];
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var values = [];
+
+var userLength;
+var userLowercase;
+var userUppercase;
+var userNumeric;
+var userChar;
 
 //Generate array for 'upperLetters' by using the array 'lowLetters'
 for(var i=0; i < lowLetters.length; i++) {
@@ -11,61 +18,63 @@ for(var i=0; i < lowLetters.length; i++) {
   upperLetters[i] = x;
 }
 
-//Prompt the user for specific criteria
-var userLength = prompt("Input the number of characters (8-128) you would like your password to be: ");
-var userLowercase = confirm("Would you like to include lowercase letters?");
-var userUppercase = confirm("Would you like to include uppercase letters?");
-var userNumeric = confirm("Would you like to include numbers?");
-var userChar = confirm("Would you like to include special characters?");
-var values = [];
-
-//Validate the user's selections and display it back
-alert("You want a password with " + userLength + " characters and the following criteria: " + "\r\n" +
-"Lowercase Letters: " + userLowercase + "\r\n" +
-"Uppercase Letters: " + userUppercase + "\r\n" +
-"Numeric Values: " + userNumeric + "\r\n" +
-"Special Characters: " + userChar );
 
 
-//Create an array with all possible characters that user has specified
-//Ensure that the user inputs a valid character length and selects at least 1 character type
-if (userLength >= 8 && userLength <= 128) {
-  if (userLowercase == false && userUppercase == false && userNumeric == false && userChar == false) {
-    alert("ERROR: Please refresh page & select at least 1 charactype for your password.");
-  }
-  else {
-
-    for(var i=0; i < lowLetters.length; i++) {
-      if (userLowercase) {
-        values.push(lowLetters[i]);
-      }
-    }
-    for(var i=0; i < upperLetters.length; i++) {
-        if (userUppercase) {
-          values.push(upperLetters[i]);
-        }
-    }
-    for(var i=0; i < numbers.length; i++) {
-        if (userNumeric){
-          values.push(numbers[i]);
-        }
-    }
-    for(var i=0; i < specialChar.length; i++) {
-        if (userChar) {
-          values.push(specialChar[i]);
-        }
-    }
-  }
-}
-else {
-  alert("ERROR: Please refresh page & input a number between 8 & 128 for the number of characters you would like.");
-}
-
-console.log("Values: " + values);
 
 
 // Passwork Generator Function
 function generatePassword() {
+  //Add all prompts:
+  //Prompt the user for specific criteria
+  userLength = prompt("Input the number of characters (8-128) you would like your password to be: ");
+  userLowercase = confirm("Would you like to include lowercase letters?");
+  userUppercase = confirm("Would you like to include uppercase letters?");
+  userNumeric = confirm("Would you like to include numbers?");
+  userChar = confirm("Would you like to include special characters?");
+
+  //Validate the user's selections and display it back
+  alert("You want a password with " + userLength + " characters and the following criteria: " + "\r\n" +
+  "Lowercase Letters: " + userLowercase + "\r\n" +
+  "Uppercase Letters: " + userUppercase + "\r\n" +
+  "Numeric Values: " + userNumeric + "\r\n" +
+  "Special Characters: " + userChar );
+  
+  //Create an array with all possible characters that user has specified
+  //Ensure that the user inputs a valid character length and selects at least 1 character type
+  if (userLength >= 8 && userLength <= 128) {
+    if (userLowercase == false && userUppercase == false && userNumeric == false && userChar == false) {
+      alert("ERROR: Please refresh page & select at least 1 charactype for your password.");
+    }
+    else {
+
+      for(var i=0; i < lowLetters.length; i++) {
+        if (userLowercase) {
+          values.push(lowLetters[i]);
+        }
+      }
+      for(var i=0; i < upperLetters.length; i++) {
+          if (userUppercase) {
+            values.push(upperLetters[i]);
+          }
+      }
+      for(var i=0; i < numbers.length; i++) {
+          if (userNumeric){
+            values.push(numbers[i]);
+          }
+      }
+      for(var i=0; i < specialChar.length; i++) {
+          if (userChar) {
+            values.push(specialChar[i]);
+          }
+      }
+    }
+  }
+  else {
+    alert("ERROR: Please refresh page & input a number between 8 & 128 for the number of characters you would like.");
+  }
+
+  console.log("Values: " + values);
+
   var A =[];
   var newWord;
 
